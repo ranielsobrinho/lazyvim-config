8a31c621a55166b93ce83b1a4bdfdf63c95d38c7
Raniel Sobrinho <raniel25sobrinho@gmail.com>
2023-12-02 11:56:33 -0300

    feat: initial commit

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,89 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+-- 1
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
+-- Increment - Decrement
+keymap.set("n", "+", "<C-a>")
+keymap.set("n", "-", "<C-x>")
+
+-- Delete a word backwards
+keymap.set("n", "dw", "vb_d")
+
+-- Select all
+keymap.set("n", "<C-a>", "gg<S-v>G")
+
+-- New tab
+keymap.set("n", "<C-n>", ":tabedit<Return>", opts)
+
+-- Split winodw
+keymap.set("n", "sh", ":split<Return>", opts)
+keymap.set("n", "sv", ":vsplit<Return>", opts)
+
+-- Neotree
+keymap.set("n", "<C-f>", ":Neotree toggle<Return>", opts)
+
+-- Easy quit
+keymap.set("n", "<C-q>", ":q!<Return>", opts)
+keymap.set("n", "<S-d>", ":bd<Return>", opts)
+
+-- ToggleTerm
+keymap.set("n", "<C-t>", ":ToggleTerm<Return>", opts)
+
+-- Show dependency versions
+keymap.set(
+  "n",
+  "<leader>ns",
+  require("package-info").show,
+  { silent = true, noremap = true, desc = "Show dependency versions" }
+)
+
+-- Hide dependency versions
+keymap.set(
+  "n",
+  "<leader>nc",
+  require("package-info").hide,
+  { silent = true, noremap = true, desc = "Hide dependency versions" }
+)
+
+-- Toggle dependency versions
+keymap.set(
+  "n",
+  "<leader>nt",
+  require("package-info").toggle,
+  { silent = true, noremap = true, desc = "Toggle dependency versions" }
+)
+
+-- Update dependency on the line
+keymap.keymap.set(
+  "n",
+  "<leader>nu",
+  require("package-info").update,
+  { silent = true, noremap = true, desc = "Update dependency on the line" }
+)
+
+-- Delete dependency on the line
+keymap.keymap.set(
+  "n",
+  "<leader>nd",
+  require("package-info").delete,
+  { silent = true, noremap = true, desc = "Delete dependency on the line" }
+)
+
+-- Install a new dependency
+keymap.keymap.set(
+  "n",
+  "<leader>ni",
+  require("package-info").install,
+  { silent = true, noremap = true, desc = "Install a new dependency" }
+)
+
+-- Install a different dependency version
+keymap.keymap.set(
+  "n",
+  "<leader>np",
+  require("package-info").change_version,
+  { silent = true, noremap = true, desc = "Install a different dependency version" }
+)
---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,12 @@
+return {
+  "akinsho/toggleterm.nvim",
+  version = "*",
+  priority = 1000,
+  opts = {
+    direction = "float",
+    close_on_exit = true,
+    float_opts = {
+      border = "curved",
+    },
+  },
+}
